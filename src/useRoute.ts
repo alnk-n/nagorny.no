9599ff054f0b7b6286dd5f3c2f9f007416def4ba
alnk-n <alnnagorny@gmail.com>
2026-05-28 08:21:11 +0200

    chore: rename useHashRoute.ts to useRoute.ts

--- src/useRoute.ts
+++ src/useRoute.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-export function useHashRoute(): [string, (p: string) => void] {
+export function useRoute(): [string, (p: string) => void] {
   const { pathname } = useLocation();
   const nav = useNavigate();
   const navigate = React.useCallback((p: string) => nav(p), [nav]);
